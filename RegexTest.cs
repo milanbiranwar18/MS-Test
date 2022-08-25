1f3ebcc2bcd8c82e877f65592db4b586cc001898
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-25 11:23:38 +0530

    Adding program for validating Email

---
+++ RegexTest.cs
@@ -0,0 +1,37 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTestAssignment;
+
+namespace MSTest
+{
+    [TestClass]
+    public class RegexTest
+    {
+        [TestMethod]
+        public void TestingFirstName()
+        {
+            Patterns pattern = new Patterns();
+            bool expected = true;
+            bool val = pattern.validateFirstName("Milan");
+            Assert.AreEqual(expected, val);
+        }
+
+        [TestMethod]
+        public void TestingLastName()
+        {
+            Patterns pattern = new Patterns();
+            bool expected = true;
+            bool val = pattern.validateLastName("Biranwar");
+            Assert.AreEqual(expected, val);
+        }
+
+        [TestMethod]
+        public void TestingEmail()
+        {
+            Patterns pattern = new Patterns();
+            bool expected = true;
+            bool val = pattern.validateEmail("milanbiranwar@gmail.com");
+            Assert.AreEqual(expected, val);
+        }
+
+    }
+}
