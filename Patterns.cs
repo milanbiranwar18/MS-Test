3f91feb800f7d0003c4bebdbd6c3ea6cea47827d
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-25 10:50:48 +0530

    Program for validating first name in ms-test

---
+++ Patterns.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MSTestAssignment
+{
+    public class Patterns
+    {
+        //UC1
+        public static string RegexFirstName = "^[A-Z]{1,}[a-z]{2,}$";
+        public bool validateFirstName(string FirstName)
+        {
+            return Regex.IsMatch(FirstName, RegexFirstName);
+        }
+    }
+}
