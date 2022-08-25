056340cb7d0b260250cdcb02686e79c16ca5ac47
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-25 10:46:31 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MSTestAssignment
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Wellcome to MS-Test Problem");
+        }
+    }
+}
+
